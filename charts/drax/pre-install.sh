a3a8282cfde781bdce3082fed507a429adb81eda
Wout Van De Wiel <wout.vandewiel@accelleran.com>
2025-07-04 11:40:52 +0200

    ci: introduce pre-install hook

---
+++ charts/drax/pre-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GIT_ROOT="$(realpath "$(dirname "$0")/../..")"
+
+# renovate: github_repository=metallb/metallb versioning=semver
+export METALLB_VERSION="v0.15.2"
+"$GIT_ROOT"/bin/install-metallb | tee /tmp/metallb.log
+metallb_ip_address="$(cat /tmp/metallb.log | grep "ip_address=" | cut -d= -f2)"
+rm /tmp/metallb.log
+
+printf "ct_install_args=--helm-extra-set-args \"--set=global.ipAddress=%s\"\n" "$metallb_ip_address" \
+    | tee -a "${GITHUB_OUTPUT:-/dev/null}"
